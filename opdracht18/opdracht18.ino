ab739cab324c6745dbec4c5f6bf16df83460d0d3
Syntasu <abouma18@hotmail.com>
2018-05-22 11:58:36 +0200

    Assignment 18

--- opdracht18/opdracht18.ino
+++ opdracht18/opdracht18.ino
@@ -5,10 +5,15 @@ Servo servo;
 void setup() 
 {
   servo.attach(3);
+
+  Serial.begin(9600);
 }
 
 void loop() 
 {
-  // put your main code here, to run repeatedly:
+  int pot = analogRead(0);
+  int mapped = map(pot, 0, 1023, 0, 180);
 
+  servo.write(mapped);
+  delay(20);
 }
